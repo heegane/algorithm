14090b20a29f46601244534cf6e20c5217fd9cfd
Hyeon Hee <101989268+heegane@users.noreply.github.com>
2023-01-07 23:57:56 +0900

    [Silver V] Title: 나이순 정렬, Time: 244 ms, Memory: 47544 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+arr = []
+
+for i in range(0, N):
+    input_value = sys.stdin.readline().split()
+    age, name = int(input_value[0]), input_value[1]
+    arr.append([age, name])
+
+arr.sort(key=lambda x: x[0])
+
+for i in range(0, N):
+    print(f"{arr[i][0]} {arr[i][1]}")
