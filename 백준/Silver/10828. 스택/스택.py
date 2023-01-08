21cf6e585d0ecd2ec8bad13279a9ed3244c9a303
Hyeon Hee <101989268+heegane@users.noreply.github.com>
2023-01-08 14:41:00 +0900

    [Silver IV] Title: 스택, Time: 68 ms, Memory: 34112 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+
+queue = deque()
+
+for i in range(N):
+    input_arr = sys.stdin.readline().split()
+    command = input_arr[0]
+    if command == "push":
+        value = int(input_arr[1])
+        queue.append(int(value))
+    else:
+        if command == "pop":
+            if len(queue) == 0:
+                print(-1)
+            else:
+                pop_value = queue.pop()
+                print(pop_value)
+        elif command == "size":
+            print(len(queue))
+        elif command == "empty":
+            if len(queue) == 0:
+                print(1)
+            else:
+                print(0)
+        elif command == "top":
+            size = len(queue)
+            if size == 0:
+                print(-1)
+            else:
+                print(f"{queue[size-1]}")
