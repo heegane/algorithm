4a953f24289d07b7781698b3b54738daf041e37e
Hyeon Hee <101989268+heegane@users.noreply.github.com>
2023-01-24 15:02:56 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 684 ms, Memory: 71348 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.setrecursionlimit(10 ** 8)
+
+
+def dfs(graph, v, visited):
+    global count
+    visited[v] = True
+    answer[v - 1] = count
+    count += 1
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+N, M, R = map(int, input().split())
+
+graph = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
+answer = [0] * N
+count = 1
+for _ in range(M):
+    first, second = sys.stdin.readline().rstrip().split()
+    graph[int(first)].append(int(second))
+    graph[int(second)].append(int(first))
+
+for edge in graph:
+    edge.sort()
+
+dfs(graph, R, visited)
+
+for result in answer:
+    sys.stdout.write(str(result) + "\n")
