f898217e8d67abe2239268745fa3f2c56a417e47
Hyeon Hee <101989268+heegane@users.noreply.github.com>
2023-01-27 15:47:17 +0900

    [Silver IV] Title: 카드, Time: 148 ms, Memory: 54552 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.py
@@ -0,0 +1,13 @@
+import sys
+
+card = {}
+n = int(input())
+for _ in range(n):
+    data = int(sys.stdin.readline().rstrip())
+    if data in card:
+        card[data] += 1
+    else:
+        card[data] = 1
+
+card = sorted(card.items(), key=lambda x: (-x[1], x[0]))
+print(card[0][0])
