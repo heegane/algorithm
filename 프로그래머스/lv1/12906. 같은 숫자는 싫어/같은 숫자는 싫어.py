fc373711a6421786efb36361a7e96d3638969563
Hyeon Hee <101989268+heegane@users.noreply.github.com>
2023-07-15 12:46:02 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 202.64 ms, Memory: 28.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,23 +1,12 @@
-from collections import deque
-
 def solution(arr):
     answer = []
     
-    deq = deque(arr)
-    
-    if len(answer) == 0:
-        answer.append(deq[0])
-        deq.popleft()
-        
-    for _ in range(len(deq)):
-        if len(deq) == 0:
-            break
-            
-        else: 
-            if answer[-1] == deq[0]:
-                deq.popleft()
-            else:
-                answer.append(deq[0])
-                deq.popleft()
-                
+    for number in arr:
+        if len(answer) == 0:
+            answer.append(number)
+            del arr[0]
+        else:
+            if answer[-1] == number:
+                del arr[0]
+                 
     return answer
