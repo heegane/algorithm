6693d4251451ce297fefe9ad1c708836622e2fef
Lee Hyeonhee <dlgusgml1314@naver.com>
2024-04-17 15:03:40 +0900

    [Gold V] Title: 두 용액, Time: 128 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+li = sorted(list(map(int, input().split())))
+
+left, right = 0, n - 1
+answer_a, answer_b = 0, 0
+cmp_v = sys.maxsize
+
+while left < right:
+    sum_v = li[left] + li[right]
+
+    if abs(sum_v) < cmp_v:
+        cmp_v = abs(sum_v)
+        answer_a, answer_b = li[left], li[right]
+        if sum_v == 0:
+            break
+
+    # 0에 가깝게 하기 위해
+    # 음수일 때
+    if sum_v < 0:
+        left += 1
+    # 양수일 때
+    else:
+        right -= 1
+
+print(answer_a, answer_b)
