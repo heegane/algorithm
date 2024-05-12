aa6b170f0d76d10a29f82a8469d6a659feb7b3cf
Lee Hyeonhee <dlgusgml1314@naver.com>
2024-05-12 23:12:58 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,26 @@
+import java.lang.Math;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int sum = brown + yellow;
+        int j = 0, r = 0, c = 0;
+        
+        for(int i = 3; i <= sum; i ++) {
+            if(sum%i == 0) {
+                j = sum / i;
+            } else {
+                continue;
+            }
+            r = Math.max(i,j);
+            c = Math.min(i,j);
+            
+            if((r-2) * (c-2) == yellow) {
+                answer[0] = r;
+                answer[1] = c;
+                return answer;
+            }
+        }
+        return answer;
+    }
+}
