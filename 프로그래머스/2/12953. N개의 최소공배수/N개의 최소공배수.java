93ed3d1888aa7f51adaa7d1467c3f35d7f850f41
Lee Hyeonhee <dlgusgml1314@naver.com>
2024-05-13 09:46:54 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.04 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,8 +1,5 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] arr) {
-        Arrays.sort(arr);
         int answer = arr[0];
         
         for(int i = 1; i < arr.length; i++) {
