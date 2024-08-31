1d8d6216ef99abb0c93e6aca530e2ff04dd7da46
Lee Hyeonhee <dlgusgml1314@naver.com>
2024-08-31 19:31:26 +0900

    [level 2] Title: 피로도, Time: 3.12 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,24 @@
+class Solution {
+    int answer;
+    int n;
+    boolean[] visited;
+    public int solution(int k, int[][] dungeons) {
+        answer = 0;
+        n = dungeons.length;
+        visited = new boolean[n];
+        dfs(k, dungeons, 0);
+        return answer;
+    }
+    
+    public void dfs(int k, int[][] dungeons, int cnt) {
+        for(int i = 0; i < n; i++) {
+            int[] dungeon = dungeons[i];
+            if(dungeon[0] <= k && !visited[i]) {
+                visited[i] = true;
+                dfs(k-dungeon[1], dungeons, cnt + 1);
+                visited[i] = false;
+            }
+        }      
+        answer = Math.max(answer, cnt);
+    }
+}
