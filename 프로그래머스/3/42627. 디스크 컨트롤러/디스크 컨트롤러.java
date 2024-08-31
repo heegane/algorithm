503d357057ea545782b8000ef28405ced5b2873d
Lee Hyeonhee <dlgusgml1314@naver.com>
2024-08-31 20:03:15 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.51 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] jobs) {
+        int totalTime = 0;
+        int curTime = 0;
+        int curIdx = 0;
+        int cnt = 0;
+        
+        Arrays.sort(jobs, (j1,j2) -> j1[0] - j2[0]);
+        Queue<int[]> pq = new PriorityQueue<>((o1,o2)->o1[1] - o2[1]);
+        
+        while(cnt < jobs.length) {
+            while(curIdx < jobs.length && curTime >= jobs[curIdx][0]) {
+                pq.add(new int[]{jobs[curIdx][0],jobs[curIdx][1]});
+                curIdx++;
+            }
+            if(pq.isEmpty()){
+                curTime = jobs[curIdx][0];
+            } else {
+                int[] job = pq.poll();
+                totalTime += job[1] + curTime - job[0];
+                curTime += job[1];
+                cnt++;
+            }
+        }
+        
+        return totalTime/jobs.length;
+    }
+}
