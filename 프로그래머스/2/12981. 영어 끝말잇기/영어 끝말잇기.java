b8bdccc9104d1d5dfb9b895223d37a1a90438c46
Hyeonhee Lee <dlgusgml1314@naver.com>
2024-11-12 19:45:20 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        Map<String, Boolean> map = new HashMap<>();
+        char pre = words[0].charAt(words[0].length() - 1);
+        map.put(words[0], true);
+
+        for(int i = 1; i < words.length; i++) {
+            String word = words[i];
+            if(pre != word.charAt(0)) {
+                answer[0] = (i + 1) % n == 0 ? n : (i + 1) % n;
+                answer[1] = (i + 1) % n == 0 ? (i + 1) / n : (i + 1) / n + 1;
+                return answer;
+            }
+            if(map.containsKey(word)) {
+                answer[0] = (i + 1) % n == 0 ? n : (i + 1) % n;
+                answer[1] = (i + 1) % n == 0 ? (i + 1) / n : (i + 1) / n + 1;
+                return answer;
+            } else {
+                map.put(word, true);
+            }
+            pre = word.charAt(word.length() - 1);
+        }
+
+        return answer;
+    }
+}
