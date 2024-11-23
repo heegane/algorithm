36e2cbf23e5f6d71fc02630dd90d38970ebb96b5
Hyeonhee Lee <dlgusgml1314@naver.com>
2024-11-23 16:23:12 +0900

    [level 1] Title: [1차] 비밀지도, Time: 14.58 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -3,31 +3,10 @@ import java.util.*;
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        int[][] graph1 = new int[n][n];
-        int[][] graph2 = new int[n][n];
-        makeGraph(graph1, arr1, n);
-        makeGraph(graph2, arr2, n);
-        
-        for(int i = 0; i < n; i++) {
-            StringBuilder result = new StringBuilder();
-            for(int j = 0; j < n; j++) {
-                if(graph1[i][j] == 1 || graph2[i][j] == 1) {
-                    result.append("#");
-                } else {
-                    result.append(" ");
-                }
-            }
-            answer[i] = result.toString();
+        for (int i = 0; i < n; i++) {
+            answer[i] = String.format("%" + n + "s", Integer.toBinaryString(arr1[i] | arr2[i])).replace(" ", "0");
+            answer[i] = answer[i].replaceAll("1", "#").replaceAll("0", " ");
         }
         return answer;
-    }
-    
-    public void makeGraph(int[][] graph, int[] arr, int n) {
-        for (int i = 0; i < arr.length; i++) {
-            String binary = String.format("%" + n + "s", Integer.toBinaryString(arr[i])).replace(' ', '0');
-            for (int j = 0; j < n; j++) {
-                graph[i][j] = binary.charAt(j) - '0'; // 각 문자 -> 숫자로 변환
-            }
-        }
     }
 }
